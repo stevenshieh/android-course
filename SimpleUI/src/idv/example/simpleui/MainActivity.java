a8d339a6d238a308012f49af9849a3a629b2b25c
stevenshieh <chirevl@gmail.com>
2014-04-24 19:31:10 +0800

    加入新的Activity
    在點擊[送出]的時候，切換到新的Activity作另一個畫面的顯示。

--- SimpleUI/src/idv/example/simpleui/MainActivity.java
+++ SimpleUI/src/idv/example/simpleui/MainActivity.java
@@ -3,6 +3,7 @@ package idv.example.simpleui;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
@@ -142,6 +143,11 @@ public class MainActivity extends ActionBarActivity {
 					.show();
 
 			text1.getText().clear();
+			
+			Intent intent = new Intent();
+			intent.setClass(getActivity(), MessageActivity.class);
+			getActivity().startActivity(intent);
+			
 		}
 	}
 
---
+++ SimpleUI/src/idv/example/simpleui/MessageActivity.java
@@ -0,0 +1,12 @@
+package idv.example.simpleui;
+
+import android.app.Activity;
+
+public class MessageActivity extends Activity {
+
+	@Override
+	protected void onChildTitleChanged(Activity childActivity,
+			CharSequence title) {
+		super.onChildTitleChanged(childActivity, title);
+	}
+}
