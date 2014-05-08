bdeb92ca7d89a74aca2fe15d26d892a8b922e74f
stevenshieh <chirevl@gmail.com>
2014-05-09 00:18:13 +0800

    作業：計算機
    等號做catch exception

--- HW_Calculator/src/idv/and23720/hw1/Operator.java
+++
@@ -1,5 +0,0 @@
-package idv.and23720.hw1;
-
-public interface Operator {
-	double calc(double first, double second);
-}
---
+++ HW_Calculator/src/idv/and23720/hw1/operator/Operator.java
@@ -0,0 +1,7 @@
+package idv.and23720.hw1.operator;
+
+import java.math.BigDecimal;
+
+public interface Operator {
+	BigDecimal calc(BigDecimal first, BigDecimal second);
+}
